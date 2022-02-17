690c9613a7f2e548673a28dfe8703f1087c7e29a
Mikis Woodwinter <Miikis@users.noreply.github.com>
2022-02-17 14:14:54 -0800

    feat: setup output file tracing

--- apps/haha/next.config.js
+++ apps/haha/next.config.js
@@ -1,10 +1,15 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withNx = require('@nrwl/next/plugins/with-nx');
+const path = require('path');
 
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  experimental: {
+    outputStandalone: true,
+    outputFileTracingRoot: path.join(__dirname, '../../'),
+  },
   nx: {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
